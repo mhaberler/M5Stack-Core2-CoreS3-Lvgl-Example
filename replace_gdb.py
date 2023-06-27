ea89012c03508aa67aca873c1cb3819d6b098938
Michael Haberler <git@mah.priv.at>
2023-06-27 14:12:30 +0200

    before clean

--- replace_gdb.py
+++ replace_gdb.py
@@ -3,5 +3,6 @@ Import ("env")
 #env.Replace(GDB="/Users/mah/espressif-gdb/xtensa-esp-elf-gdb/bin/xtensa-esp32s3-elf-gdb")
 
 
-env.Replace(GDB="/Users/mah/Downloads/xtensa-esp-elf-gdb/bin/xtensa-esp32s3-elf-gdb")
-
+# env.Replace(GDB="/Users/mah/Downloads/xtensa-esp-elf-gdb/bin/xtensa-esp32s3-elf-gdb")
+#  "/Users/mah/Downloads/xtensa-esp-elf-gdb/bin",
+# toolchainBinDir
