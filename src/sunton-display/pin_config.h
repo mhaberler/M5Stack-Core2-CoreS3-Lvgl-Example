a06fb585a2fb39aaca0e4875025efaae5152a474
Michael Haberler <git@mah.priv.at>
2023-06-27 06:07:22 +0200

    sunton: carry over source from broken branch

---
+++ src/sunton-display/pin_config.h
@@ -0,0 +1,46 @@
+// #pragma once
+
+// /**--------------------------------------------------------------------------------------
+//  * ESP32S3 Sunton 7in 
+//  * source:
+//  * ESP32-8048S070-LCM-V1.1.png
+//  * ESP32-8048S070-MCU-V1.1.png
+//  *-------------------------------------------------------------------------------------*/
+
+// // Button pins
+// // #define PIN_BUTTON_1 0
+// // #define PIN_BUTTON_2 14
+
+// // Battery voltage pin
+// // #define PIN_BAT_VOLT 4
+
+// // External expansion sd card pins
+// #define PIN_SD_CMD 11  // MOSI
+// #define PIN_SD_CLK 12
+// // TF CS = 10
+// // TF MISO = 13
+
+// // #define PIN_SD_D0 12
+
+// // Touch pad pins
+// #define PIN_IIC_SCL 20
+// #define PIN_IIC_SDA 19
+// #define PIN_TOUCH_INT 
+// #define PIN_TOUCH_RES 21
+
+// // Display pins
+// #define PIN_LCD_POWER_ON 15
+// #define PIN_LCD_RES 5
+// #define PIN_LCD_CS 6
+// #define PIN_LCD_DC 7
+// #define PIN_LCD_WR 8
+// #define PIN_LCD_RD 9
+// #define PIN_LCD_BL 38
+// #define PIN_LCD_D0 39
+// #define PIN_LCD_D1 40
+// #define PIN_LCD_D2 41
+// #define PIN_LCD_D3 42
+// #define PIN_LCD_D4 45
+// #define PIN_LCD_D5 46
+// #define PIN_LCD_D6 47
+// #define PIN_LCD_D7 48
