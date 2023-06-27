f6abe26f435e0bc6bae8bec361b4032b17562623
Michael Haberler <git@mah.priv.at>
2023-06-27 19:26:17 +0200

    add hello-world to nail versions

---
+++ src/hello-world/main.cpp
@@ -0,0 +1,30 @@
+
+#ifdef M5UNIFIED
+#include <M5Unified.h>
+#else
+#include <Arduino.h>
+#endif
+#include "ui/lv_setup.h"
+#include "ui/ui.h"
+
+#include "Esp.h"
+
+void setup() {
+#ifdef M5UNIFIED
+    auto cfg = M5.config();
+    M5.begin(cfg);
+#else
+    Serial.begin(115200);
+#endif
+    while (!Serial) {
+        yield();
+    }
+    Serial.printf("Hello, World!\n");
+}
+
+void loop() {
+    delay(1000);
+    Serial.printf("Hello, World loop!\n");
+    Serial.printf("Total PSRAM=%u\n", ESP.getPsramSize());
+    Serial.printf("Free PSRAM=%u\n", ESP.getFreePsram());
+}
---
+++ src/hello-world/pin_config.h
@@ -0,0 +1,6 @@
+// Touch pad pins
+#define PIN_IIC_SCL -1
+#define PIN_IIC_SDA -1
+#define PIN_TOUCH_INT -1
+#define PIN_TOUCH_RES -1
+#define PIN_LCD_POWER_ON -1
